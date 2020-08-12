3dd41aa0feb36b4ff0fdaba40433db51685393bf
rs <r.simons@sma-partner.ch>
2020-08-12 11:03:46 +0200

    Trigger SetCopyLocalForSolution if we suspect a reference was added
    - currently for all items added to any project that have no type guid

--- VSCopyLocal/VSCopyLocal/VSCopyLocalPackage.cs
+++ VSCopyLocal/VSCopyLocal/VSCopyLocalPackage.cs
@@ -11,6 +11,7 @@ using System.Threading;
 using VSUtils;
 using VSUtils.Project;
 using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
 
 namespace CodeValue.VSCopyLocal
@@ -35,10 +36,17 @@ namespace CodeValue.VSCopyLocal
     [Guid(GuidList.GuidVsPackageTemplatePkgString)]
     [ProvideOptionPage(typeof(OptionsPage), "VSCopyLocal", "Settings", 0, 0, true)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
-    public sealed class VSCopyLocalPackage : AsyncPackage
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class VSCopyLocalPackage : AsyncPackage, IVsSolutionEvents
     {
         private static DTE _dte;
         private static OptionsPage _options;
+
+        private IVsSolution _solution;
+        private uint _hSolutionEvents = uint.MaxValue;
+        private IList<HierarchyHandler> _hierarchyHandlers = new List<HierarchyHandler>();
+
 
         private const string CActionTextFormat = "Turn {0} Copy Local";
         private const string CProjectActionTextFormat = CActionTextFormat + " for this Project";
@@ -50,6 +58,8 @@ namespace CodeValue.VSCopyLocal
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress) {
+            await AdviseSolutionEvents();
+
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
             // Add our command handlers for menu (commands must exist in the .vsct file)
@@ -75,8 +85,59 @@ namespace CodeValue.VSCopyLocal
             mcs.AddCommand(menuItem);
             
             _dte = (DTE)GetGlobalService(typeof(DTE));
-
             _options = GetDialogPage(typeof(OptionsPage))as OptionsPage;
+        }
+
+        protected override void Dispose(bool disposing) {
+            UnadviseSolutionEvents();
+            foreach (var hierarchyHandler in _hierarchyHandlers) {
+                hierarchyHandler.Unadvise();
+            }
+            _hierarchyHandlers.Clear();
+
+            base.Dispose(disposing);
+        }
+
+        private async Task AdviseSolutionEvents() {
+            _solution = (IVsSolution) await GetServiceAsync(typeof(SVsSolution));
+            var projectsInSolution = GetProjectsInSolution(_solution);
+            foreach (var project in projectsInSolution) {
+                AddHierarchyHandler(project);
+            }
+
+            _solution.AdviseSolutionEvents(this, out _hSolutionEvents);
+        }
+
+
+        private static IEnumerable<IVsHierarchy> GetProjectsInSolution(IVsSolution solution) {
+            __VSENUMPROJFLAGS flags = __VSENUMPROJFLAGS.EPF_LOADEDINSOLUTION;
+            Guid guid = Guid.Empty;
+            IEnumHierarchies enumHierarchies;
+            solution.GetProjectEnum((uint)flags, ref guid, out enumHierarchies);
+            if (enumHierarchies == null) {
+                yield break;
+            }
+
+            IVsHierarchy[] hierarchies = new IVsHierarchy[1];
+
+            uint fetched;
+            while (enumHierarchies.Next(1, hierarchies, out fetched) == VSConstants.S_OK && fetched == 1) {
+                if (hierarchies.Length > 0 && hierarchies[0] != null) {
+                    yield return hierarchies[0];
+                }
+            }
+        }
+
+        private void UnadviseSolutionEvents() {
+            if (_solution != null) {
+                if (_hSolutionEvents != uint.MaxValue) {
+                    _solution.UnadviseSolutionEvents(_hSolutionEvents);
+
+                    _hSolutionEvents = uint.MaxValue;
+                }
+
+                _solution = null;
+            }
         }
 
         private static void OnBeforeQueryStatus(object sender, EventArgs e)
@@ -176,6 +237,124 @@ namespace CodeValue.VSCopyLocal
             if (cmd.CommandID.ID.Equals((int)PkgCmdIDList.CmdidReferencesCommand)) // references node
             {
                 cmd.Text = string.Format(CActionTextFormat, action);
+            }
+        }
+
+        int IVsSolutionEvents.OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded) {
+            AddHierarchyHandler(pHierarchy);
+
+            return VSConstants.S_OK;
+        }
+
+        private void AddHierarchyHandler(IVsHierarchy pHierarchy) {
+            var handler = new HierarchyHandler(pHierarchy);
+
+            uint handlerCode;
+            pHierarchy.AdviseHierarchyEvents(handler, out handlerCode);
+            handler.SetCode(handlerCode);
+            _hierarchyHandlers.Add(handler);
+        }
+
+        int IVsSolutionEvents.OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterOpenSolution(object pUnkReserved, int fNewSolution) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnQueryCloseSolution(object pUnkReserved, ref int pfCancel) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseSolution(object pUnkReserved) {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterCloseSolution(object pUnkReserved) {
+            foreach (var hierarchyHandler in _hierarchyHandlers) {
+                hierarchyHandler.Unadvise();
+            }
+            _hierarchyHandlers.Clear();
+
+            return VSConstants.S_OK;
+        }
+
+        private class HierarchyHandler : IVsHierarchyEvents {
+            private readonly IVsHierarchy _hierarchy;
+            private uint _handler;
+
+            public HierarchyHandler(IVsHierarchy hierarchy) {
+                _hierarchy = hierarchy;
+            }
+
+            public void SetCode(uint code) {
+                _handler = code;
+            }
+
+            public bool MatchesHierarchy(IVsHierarchy hierarchy) {
+                return ReferenceEquals(hierarchy, _hierarchy);
+            }
+
+            public void Unadvise() {
+                _hierarchy.UnadviseHierarchyEvents(_handler);
+            }
+
+            public int OnItemAdded(uint itemidParent, uint itemidSiblingPrev, uint itemidAdded) {
+                string name;
+                _hierarchy.GetCanonicalName(itemidAdded, out name);
+
+                object propName;
+                var propNameResult = _hierarchy.GetProperty(itemidAdded, (int)__VSHPROPID.VSHPROPID_Name, out propName);
+
+                Guid propTypeGuid;
+                var propTypeGuidResult = _hierarchy.GetGuidProperty(itemidAdded, (int)__VSHPROPID.VSHPROPID_TypeGuid, out propTypeGuid);
+
+                // TODO: is there a better way to find out if this is a reference? 
+                if (propNameResult == VSConstants.S_OK && propTypeGuidResult == VSConstants.S_OK) {
+                    if (!string.IsNullOrEmpty((string)propName) && propTypeGuid == Guid.Empty) {
+                        // This could have been a reference!
+                        // TODO: only change this project, or even only this reference!
+                        SetCopyLocalForSolution();
+                    }
+                }
+                return VSConstants.S_OK;
+            }
+
+            public int OnItemsAppended(uint itemidParent) {
+                return VSConstants.S_OK;
+            }
+
+            public int OnItemDeleted(uint itemid) {
+                return VSConstants.S_OK;
+            }
+
+            public int OnPropertyChanged(uint itemid, int propid, uint flags) {
+                return VSConstants.S_OK;
+            }
+
+            public int OnInvalidateItems(uint itemidParent) {
+                return VSConstants.S_OK;
+            }
+
+            public int OnInvalidateIcon(IntPtr hicon) {
+                return VSConstants.S_OK;
             }
         }
     }
